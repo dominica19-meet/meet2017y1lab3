08950566701053b2786b535d293e14de2a1c40ab
dominica19-meet <dominica19@meet.mit.edu>
2017-07-25 12:45:58 +0300

    Stopping here

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print("hello world is how you say hello world in english")
+print("Γειά σου Κόσμε is how you say hello world in greek")
+print("Olá Mundo is the way you say hello world in portuguese")
+print("Ahoj světe is the way you say hello world in cezech")
+print("안녕 세상 is th way you say hello world in korean")
+print("你好，世界 is the way you say hello world in chinise")
+print("नमस्ते दुनिया is the way you say hello world in hindi")
+print("Salamu, Dunia is the way you say hello world in swahili")
